12deb7231129e1def62d5a7bd13c915dec85a1e8
Mériadec Denier d'Aprigny <113612538+Meeria@users.noreply.github.com>
2026-02-03 16:01:47 +0100

    edit pipeline cypress #2

--- frontend/src/pages/TicketListPage.tsx
+++ frontend/src/pages/TicketListPage.tsx
@@ -65,6 +65,7 @@ export default function TicketListPage() {
             <Link id={`ticket-link-${t.id}`} to={`/tickets/${t.id}`}>Ouvrir</Link>
           </div>
           <p className="small" style={{ marginTop: 6 }}>{t.description}</p>
+          <p>test</p>
         </div>
       ))}
     </div>
